cb06cb707e28db7dfe26a0076700448fcb27e27d
Maxi Rebolo <maximilianorebolo@gmail.com>
2020-02-04 00:10:30 -0300

    Container Animated agregado/Modificaciones varias

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_components/src/pages/avatar_page.dart';
+import 'package:flutter_components/src/pages/container_animated.dart';
 
 import 'package:flutter_components/src/pages/home_page.dart';
 
@@ -9,9 +10,10 @@ import 'package:flutter_components/src/pages/card_page.dart';
 
 Map<String, WidgetBuilder> getApplicationContext(){
   return <String, WidgetBuilder> {
-    "/"       : (BuildContext context) => HomePage(),
-    "alert"   : (BuildContext context) => AlertPage(),
-    "avatars" : (BuildContext context) => AvatarPage(),
-    "cards"   : (BuildContext context) => CardPage()
+    "/"                  : (BuildContext context) => HomePage(),
+    "alert"              : (BuildContext context) => AlertPage(),
+    "avatars"            : (BuildContext context) => AvatarPage(),
+    "cards"              : (BuildContext context) => CardPage(),
+    "animated_container" : (BuildContext context) => AnimatedContainerPage(),
   };
 }
